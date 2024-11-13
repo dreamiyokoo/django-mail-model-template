00916afc8d0a75183ac0f5b6308c3bd4ef064a00
yokoo <yokoo@dreami.jp>
2024-11-14 00:59:20 +0900

    Refactor dependencies and add CI workflow
    Removed `tox-requirements.txt` and integrated dependencies into `setup.py` and `requirements.txt` for better dependency management. Added GitHub Actions workflow to automate testing and publishing on code changes and releases.

--- setup.py
+++ setup.py
@@ -3,46 +3,60 @@ import sys
 from setuptools import setup, find_packages
 
 DESCRIPTION = "Manage email templates in DB with django"
-VERSION = '0.0.1'
+VERSION = "0.0.1"
 LONG_DESCRIPTION = None
 try:
-    LONG_DESCRIPTION = open('README.rst').read()
+    LONG_DESCRIPTION = open("README.rst").read()
 except:
     pass
 
-requirements = [
+# プロジェクトの依存関係
+install_requires = [
+    "Django>=3.2",
 ]
 
+# 開発・テスト用の依存関係
+extras_require = {
+    "dev": [
+        "pytest",
+        "pytest-django",
+        "pytest-pythonpath",
+        "tox",
+        "factory_boy",
+    ]
+}
+
 # python setup.py publish
-if sys.argv[-1] == 'publish':
+if sys.argv[-1] == "publish":
     os.system("python setup.py sdist upload")
     sys.exit()
 
 CLASSIFIERS = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'Framework :: Django',
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Framework :: Django",
 ]
 
 setup(
-    name='django-mail-model-template',
+    name="django-mail-model-template",
     version=VERSION,
     packages=find_packages(exclude=("tests", "tests.*")),
     include_package_data=True,
-    author='Minoru Yokoo',
-    author_email='yokoo@dreami.jp',
-    url='https://github.com/dreamiyokoo/djano-mail-model-template',
-    license='MIT',
+    author="Minoru Yokoo",
+    author_email="yokoo@dreami.jp",
+    url="https://github.com/dreamiyokoo/djano-mail-model-template",
+    license="MIT",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/x-rst',
-    platforms=['any'],
+    long_description_content_type="text/x-rst",
+    platforms=["any"],
     classifiers=CLASSIFIERS,
-    install_requires=requirements,
+    install_requires=install_requires,
+    extras_require=extras_require,
 )
